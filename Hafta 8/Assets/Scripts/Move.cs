edec36c1fea321aad2b31f0908d6cb02b4b5aeac
Beyza Kütük <beyzakutuk55@gmail.com>
2023-09-03 01:26:04 +0300

    Update Move.cs

--- Hafta 8/Assets/Scripts/Move.cs
+++ Hafta 8/Assets/Scripts/Move.cs
@@ -7,17 +7,29 @@ public class Move : MonoBehaviour
     private Rigidbody rb;
     public float speed = 1.5f;
     bool _continue = true;
+    public float timeCount = 500;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        PlayerPrefs.SetFloat("timeCount", timeCount);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(_continue)
+        {
+            timeCount -= Time.deltaTime;
+            PlayerPrefs.SetFloat("timeCount", timeCount);
+            print(timeCount);
+        }
+        if(timeCount < 0)
+        {
+            _continue = false;
+        }
         
     }
 
@@ -31,22 +43,22 @@ public class Move : MonoBehaviour
             Vector3 kuvvet = new(z, 0, -1 * x);
             rb.AddForce(kuvvet * speed);
 
-            if(PlayerPrefs.GetFloat("can") == 0)
-            {
-                _continue = false;
-            }
         }
         else
         {
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
         }
+        if (PlayerPrefs.GetFloat("can") == 0)
+        {
+            _continue = false;
+        }
         
+
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        float count = 20;
         string _tag = other.gameObject.tag;
         if(_tag.Equals("finishLine"))
         {
